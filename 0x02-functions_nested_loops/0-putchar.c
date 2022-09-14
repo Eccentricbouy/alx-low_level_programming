184949787131e67c9a75a181f1ec44a11728e253
Eccentricbouy <111117466+Eccentricbouy@users.noreply.github.com>
2022-09-14 05:45:51 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,14 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabets in lower case as a message.
+ * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	
-  void print_alphabet(void);
-  int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
+ printf("_putchar\n");
+	
 	return (0);
 }
