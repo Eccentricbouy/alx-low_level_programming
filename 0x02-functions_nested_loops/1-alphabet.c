273a3dc10c457ca7096a11d14168daf6dfad9903
Eccentricbouy <111117466+Eccentricbouy@users.noreply.github.com>
2022-09-14 13:05:03 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
  *main - check the code
  *Return: Always 0
  */
-
 #include <stdio.h>
 void print_alphabet(void)
 {
 char ch = 'a';
-for(ch = 'a'; ch <= 'z'; ch++)	
+for(ch = 'a'; ch <= 'z'; ch++)
 {
 		_putchar(ch);
 	}
