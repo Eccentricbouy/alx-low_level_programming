20153393ba169bcffdf6222eeb3abaf67cc9cb11
Eccentricbouy <111117466+Eccentricbouy@users.noreply.github.com>
2022-09-19 11:13:45 +0200

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @i: memeber
+ * Return: Always 0.
+ */
+int print_last_digit(i)
+{
+int l;
+l = n % 10;
+if (l < 0)
+{
+_putchar(-l + 48);
+return (-l);
+}
+else
+{
+_putchar(l + 48);
+return (l);
+}
+}
