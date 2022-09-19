d237416da61cadee6f3a3f8fd69b4bdc5a4c96ae
Eccentricbouy <111117466+Eccentricbouy@users.noreply.github.com>
2022-09-19 10:57:19 +0200

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - void
+* Return: 1, or 0
+* @c: character
+* _isalpha - chcks for alphabetic char
+*/
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
