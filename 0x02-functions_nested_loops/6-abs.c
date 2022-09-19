7af750eafca57b12a981180e458aa8acec1be1d2
Eccentricbouy <111117466+Eccentricbouy@users.noreply.github.com>
2022-09-19 11:10:33 +0200

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ * @i: member
+ * Return: Always 0.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
