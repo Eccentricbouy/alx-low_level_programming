101e2484d0cb4202cce1c6965cd6bea3388c23c5
Eccentricbouy <111117466+Eccentricbouy@users.noreply.github.com>
2022-09-19 03:52:44 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 48; i < 58; i++)
+int i;
+for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
 {
-		for (i = 97; i < 103; i++)
+for (i = 97; i < 103; i++)
 }
 {
-  putchar(i);
+putchar(i);
 }
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
