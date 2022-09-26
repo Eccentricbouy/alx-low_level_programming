afcf983c89482bef8ffbb103397b79db78abe8df
Eccentricbouy <blessingtobane@gmail.com>
2022-09-25 23:03:27 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * Return: pointer to dest
+ * @*dest: dest
+ * @*src: src
+ * @n: value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
